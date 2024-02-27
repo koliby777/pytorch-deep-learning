efdc97abbccb344a6a6394ed39e498e52a2e625b
Pavel S <drnovice777@centrum.cz>
2024-02-28 00:39:07 +0100

    01 lekce

---
+++ 01 Lekce/data.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn # nn contains all of PyTorch's building blocks for neural networks
+import matplotlib.pyplot as plt
+
+# Check PyTorch version
+print(f"torch.__version__ = {torch.__version__}")
+
+
+
