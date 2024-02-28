939ce2a7840189c71d31595cc1490e16eacdc37e
Pavel S <drnovice777@centrum.cz>
2024-02-28 01:12:55 +0100

    Update data.py

--- 01 Lekce/data.py
+++ 01 Lekce/data.py
@@ -5,5 +5,17 @@ import matplotlib.pyplot as plt
 # Check PyTorch version
 print(f"torch.__version__ = {torch.__version__}")
 
+# Create *known* parameters
+weight = 0.7
+bias = 0.3
+
+# Create data
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+print(f"{X[:10]} \n{y[:10]}")
 
 
