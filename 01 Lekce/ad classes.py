d54a118978fc688df269aa98bebb63ac35adcdca
Pavel S <drnovice777@centrum.cz>
2024-02-28 20:48:22 +0100

    Create ad classes.py

---
+++ 01 Lekce/ad classes.py
@@ -0,0 +1,24 @@
+class Parent:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+print(" Parent:")
+nic = Parent("Rodic", "Velky") 
+nic.printname()
+
+class Child(Parent):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+print("\n Child:")
+x = Child("Dite", "Male", 2024)
+x.printname() # tato fce je zdedena
+x.welcome()
