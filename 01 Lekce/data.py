e3456d7035248c153f3ecf2213d4482d9489db1c
Pavel S <drnovice777@centrum.cz>
2024-02-28 02:06:02 +0100

    Update data.py

--- 01 Lekce/data.py
+++ 01 Lekce/data.py
@@ -16,6 +16,39 @@ step = 0.02
 X = torch.arange(start, end, step).unsqueeze(dim=1)
 y = weight * X + bias
 
-print(f"{X[:10]} \n{y[:10]}")
+print(f"{X[:10], X.shape} \n {y[:10], y.shape}")
+
+# Create train/test split
+train_split = int(0.8 * len(X)) # 80% of data used for training set, 20% for testing 
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(10, 7))
+
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+  
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+  if predictions is not None:
+    # Plot the predictions in red (predictions were made on the test data)
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+  # Show the legend
+  plt.legend(prop={"size": 14});
+
+plot_predictions();
+
 
 
